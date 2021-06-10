9dd256496819e54aa7bae69d2d184875b95d4277
Sean Russell <emailseanrussell@gmail.com>
2021-06-10 00:25:45 -0700

    adding local storage requirement to project; worked past deadline; going to have to submit late

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 
 //Pseudocode:
-// display the current day at the top of the calender
-// create time blocks
+
 // color code the time blocks
     // figure out what is the time now.
     // compare the time with each block.
@@ -13,39 +12,81 @@
     // sync with storage
     let container = document.getElementById('container');
     let currentDay = document.getElementById('currentDay');
-    // console.log(moment().format("MMM Do YY"));
-    // function createTimeBlocks(){
-    //     for (let i = 9; i <= 17; i++) {
-    //         console.log(i+':00'); 
-    //     }
-    // };
-    // createTimeBlocks();
-    ///////////////////////
+    let currentHour = (moment().hour());
+    let textArea = document.getElementById('lineItem');
+    let blox = document.getElementsByClassName('input-group');
+
+
     currentDay.append(moment().format('MMMM Do YYYY'));
     console.log(moment().format('dddd'));
-    function createTimeBlocks(){
-        
-        // 9:00 - 17:00
-        for (let i = 9; i <= 17; i++) {
-            container.append(i + ':00');
-            console.log(i + ':00');
-        };
-    };
-    
+    console.log(textArea.className);
+    console.log(currentHour);
 
-        createTimeBlocks();
-        function saveToSomeSlot(){
-            // super important please watch the !!!!lecture office hours!!!!
-            // if first time user init the database
-            // if not get the existing data base
-            if(!localStorage.getItem('scheduele')){
-                localStorage.setItem('scheduele', 'New user - welcome!');
-            }
-            alert(`Here is the scheduele for today: ${localStorage.getItem('scheduele')}`)
-            const choice = prompt('Which entry would you like to update?')
-            localStorage.setItem('scheduele',choice);
-            // update the entry
-        }
-        saveToSomeSlot();
-        
-     
+    // local storage producing
+    document.getElementById('text9am').innerHTML = localStorage.getItem('text9am');
+    document.getElementById('text10am').innerHTML = localStorage.getItem('text10am');
+    document.getElementById('text11am').innerHTML = localStorage.getItem('text11am');
+    document.getElementById('text12am').innerHTML = localStorage.getItem('text12am');
+    document.getElementById('text1pm').innerHTML = localStorage.getItem('text1pm');
+    document.getElementById('text2pm').innerHTML = localStorage.getItem('text2pm');
+    document.getElementById('text3pm').innerHTML = localStorage.getItem('text3pm');
+    document.getElementById('text4pm').innerHTML = localStorage.getItem('text4pm');
+
+
+// event listeners and local storage for each line
+    document.getElementById("bttn9").addEventListener("click", function() {
+        console.log(text9am.value);
+        let entry9am = text9am.value;
+        console.log(entry9am);
+        localStorage.setItem('text9am',entry9am);
+      });    
+    document.getElementById("bttn10").addEventListener("click", function() {
+        console.log(text10am.value);
+        let entry10am = text10am.value;
+        console.log(entry10am);
+        localStorage.setItem('text10am',entry10am);
+      });
+      document.getElementById("bttn11").addEventListener("click", function() {
+        console.log(text11am.value);
+        let entry11am = text11am.value;
+        console.log(entry11am);
+        localStorage.setItem('text11am',entry11am);
+      });
+      document.getElementById("bttn12").addEventListener("click", function() {
+        console.log(text12am.value);
+        let entry12am = text12am.value;
+        console.log(entry12am);
+        localStorage.setItem('text12am',entry12am);
+      });
+      document.getElementById("bttn13").addEventListener("click", function() {
+        console.log(text1pm.value);
+        let entry1pm = text1pm.value;
+        console.log(entry1pm);
+        localStorage.setItem('text1pm',entry1pm);
+      });
+      document.getElementById("bttn14").addEventListener("click", function() {
+        console.log(text2pm.value);
+        let entry2pm = text2pm.value;
+        console.log(entry2pm);
+        localStorage.setItem('text2pm',entry2pm);
+      });
+      document.getElementById("bttn15").addEventListener("click", function() {
+        console.log(text3pm.value);
+        let entry3pm = text3pm.value;
+        console.log(entry3pm);
+        localStorage.setItem('text3pm',entry3pm);
+      });
+      document.getElementById("bttn16").addEventListener("click", function() {
+        console.log(text4pm.value);
+        let entry4pm = text4pm.value;
+        console.log(entry4pm);
+        localStorage.setItem('text4pm',entry4pm);
+      });
+      document.getElementById("bttn17").addEventListener("click", function() {
+        console.log(text5pm.value);
+        let entry5pm = text5pm.value;
+        console.log(entry5pm);
+        localStorage.setItem('text5pm',entry5pm);
+      });
+    
+    
